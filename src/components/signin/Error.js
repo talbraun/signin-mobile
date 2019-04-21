c4c8c4a1b25740225a84b4f4cbee40f774acd62c
tal braun <talbraun@gmail.com>
2019-04-21 08:05:58 +0300

    B

---
+++ src/components/signin/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Error = props => (
+  <div className="header">
+   Error Link
+  </div>
+);
+
+export default Error;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,58 @@
 import React,{ Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter,Route,Switch,Redirect} from 'react-router-dom'
 //import Button from "@material-ui/core/Button";
 //import Header from "./components/signin/Header";
 import Main from "./components/signin/Main";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
+import Error from "./components/signin/Error"
 
 class App extends Component  {
 
 
 state = {
+
   emailValue:'',
-  passwordValue:''
+  passwordValue:'',
+  toCourseId:false
+
 }
 
 setEmail = (e) =>{this.setState({emailValue:e.target.value})}
 setPassword = (e) =>{this.setState({password:e.target.value})}
 
-logIn= () =>{
-//פה תכניס את הקריאה לשרת
-//הערכים נמצאים בסטייט
-//בהצלחה וחג שמח סלים, ד"ש לעופר והדס
 
+logIn= () =>{
+  
+this.setState(()=>({toCourseId:true}))
 }
+
+
+
 render() {
+
+  if(this.state.toCourseId === true){return <Redirect to='/error'/>}
+  
   return (
     <div className="App">
+   
+       <Route path='/error' component={Error}/>
+       <Route 
+        path='/'
+        render={(props) => <Main {...props}
+          email={e =>(this.setEmail(e))}
+          password={e =>{this.setPassword(e)}}
+          logIn = {this.logIn}/>}
+        exact/>
+
+
+
       
-      <Main
-      email={e =>(this.setEmail(e))}
-      password={e =>{this.setPassword(e)}}
-      logIn = {this.logIn()}
-      />
     </div>
   )}
   ;}
 
-
+const app =(<BrowserRouter><Switch><App /></Switch></BrowserRouter>)
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(app, rootElement);
